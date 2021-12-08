6fd108178064468997b93e777410d58ff557719f
W1F1snow <dimitar.hristijan.najdovski@gmail.com>
2021-12-08 11:36:30 +0100

    Bugtracker_API_02 (I feel my iq dropping, had to recreate the same project in a different file because sts decided "main function doesnt exist" and "pom.xml doesnt exist" for some reason)

---
+++ src/main/java/com/bugtracker/BugtrackerApi1Application.java
@@ -0,0 +1,13 @@
+package com.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugtrackerApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugtrackerApi1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/bugtracker/db/UsersController.java
@@ -0,0 +1,50 @@
+package com.bugtracker.db;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersController {
+	
+    @Autowired
+    UsersRepository usersRepository;
+    
+    @GetMapping("/users/all")
+    public List<User> getAllUsers() {
+        return usersRepository.findAll();
+    }
+    
+    //syntax localhost:8080/hi?name=nameofthing (without "")
+    @ResponseBody
+    @GetMapping("/users")
+    public Optional<User> getUser(@RequestParam Integer id) {
+    	return usersRepository.findById(id);
+    }
+
+    @PostMapping("/users")
+    public void addUser(@RequestBody User user) {
+    	usersRepository.save(user);
+    }
+
+    @PutMapping("/users")
+    public void editUser(@RequestBody User user) {
+    	usersRepository.save(user);
+    }
+    
+    @ResponseBody
+    @DeleteMapping("/users")
+    public void removeUser(@RequestParam Integer id) {
+    	usersRepository.deleteById(id);
+    }
+}
